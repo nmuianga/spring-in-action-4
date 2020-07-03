3ac7b205f98fda6bcfe6f85c667e79662e99f7f8
Nilvandro <nbcmuianga@gmail.com>
2020-07-03 05:30:17 +0200

    Wiring beans with Java

--- chapter02-wiring-beans/src/main/java/mz/co/muianga/config/CDPlayerConfig.java
+++ chapter02-wiring-beans/src/main/java/mz/co/muianga/config/CDPlayerConfig.java
@@ -1,16 +1,28 @@
 package mz.co.muianga.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import mz.co.muianga.soundsystem.CDPlayer;
 import mz.co.muianga.soundsystem.CompactDisc;
+import mz.co.muianga.soundsystem.MediaPlayer;
+import mz.co.muianga.soundsystem.SgtPeppers;
 
 /**
  * @author Nilvandro Muianga, Jul 3, 2020
  *
  */
 @Configuration
-@ComponentScan (basePackageClasses = {CompactDisc.class})
 public class CDPlayerConfig {
+	
+	@Bean
+	public CompactDisc sgtPeppers() {
+		return new SgtPeppers();
+	}
+	
+	@Bean
+	public MediaPlayer cdPlayer() {
+		return new CDPlayer(sgtPeppers());
+	}
 
 }
